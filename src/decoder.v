926b7f6bc6eb999a67d57975e0d69a3e99a25298
Abonite <378751910@qq.com>
2023-01-08 22:47:47 +0800

    add full instructions and debugging

--- src/decoder.v
+++ src/decoder.v
@@ -110,17 +110,50 @@ module decoder (
         );
         begin
             case (datamux)
-                `NOP:       next_state  = INST;
-                `LOAD:      next_state  = TWO_ARGA;
-                `MOV_RR:    next_state  = TWO_ARGA;
-                `MOV_RA:    next_state  = IO_ARGA;
-                `PUSH:      next_state  = IO_ONE_ARG;
-                `POP:       next_state  = IO_ONE_ARG;
-                `ADD:       next_state  = TWO_ARGA;
-                `JMP:       next_state  = ONE_ARG;
-                `INT:       next_state  = IO_OP;
-                `SAVEPC:    next_state  = IO_OP;
-                `RECOPC:    next_state  = IO_OP;
+                `NOP:     next_state    =    INST;
+                `MOV_AR:  next_state    =    IO_ARGA;
+                `MOV_RR:  next_state    =    TWO_ARGA;
+                `MOV_RA:  next_state    =    IO_ARGA;
+                `LOAD:    next_state    =    TWO_ARGA;
+                `ADD_RR:  next_state    =    TWO_ARGA;
+                `ADD_RI:  next_state    =    TWO_ARGA;
+                `SUB_RR:  next_state    =    TWO_ARGA;
+                `SUB_RI:  next_state    =    TWO_ARGA;
+                `AND_RR:  next_state    =    TWO_ARGA;
+                `AND_RI:  next_state    =    TWO_ARGA;
+                `OR_RR:   next_state    =    TWO_ARGA;
+                `OR_RI:   next_state    =    TWO_ARGA;
+                `NOT_RR:  next_state    =    TWO_ARGA;
+                `NOT_RI:  next_state    =    TWO_ARGA;
+                `XOR_RR:  next_state    =    TWO_ARGA;
+                `XOR_RI:  next_state    =    TWO_ARGA;
+                `RAND_R:  next_state    =    TWO_ARGA;
+                `RAND_I:  next_state    =    TWO_ARGA;
+                `ROR_R:   next_state    =    TWO_ARGA;
+                `ROR_I:   next_state    =    TWO_ARGA;
+                `RXOR_R:  next_state    =    TWO_ARGA;
+                `RXOR_I:  next_state    =    TWO_ARGA;
+                `LSL_R:   next_state    =    ONE_ARG;
+                `LSL_I:   next_state    =    TWO_ARGA;
+                `LSR_R:   next_state    =    ONE_ARG;
+                `LSR_I:   next_state    =    TWO_ARGA;
+                `ASL_R:   next_state    =    ONE_ARG;
+                `ASL_I:   next_state    =    TWO_ARGA;
+                `ASR_R:   next_state    =    ONE_ARG;
+                `ASR_I:   next_state    =    TWO_ARGA;
+                `CSL_R:   next_state    =    ONE_ARG;
+                `CSL_I:   next_state    =    TWO_ARGA;
+                `CSR_R:   next_state    =    ONE_ARG;
+                `CSR_I:   next_state    =    TWO_ARGA;
+                `INC:     next_state    =    TWO_ARGA;
+                `DEC:     next_state    =    TWO_ARGA;
+                `JMP:     next_state    =    TWO_ARGA;
+                `PUSH:    next_state    =    IO_ONE_ARG;
+                `POP:     next_state    =    IO_ONE_ARG;
+                `INT:     next_state    =    IO_OP;
+                `SAVEPC:  next_state    =    INST;
+                `RECOPC:  next_state    =    INST;
+                default: next_state = 16'h0;
             endcase
         end
     endtask
@@ -290,28 +323,265 @@ module decoder (
             end
             TWO_ARGB: begin
                 case (inst)
-                    `LOAD: begin
-                        io_control_code = 2'b00;
-                        pc_control_code = 3'b010;
-                        dc_control_code = 4'b0010;
-                        ct_control_code = 13'b0;
-                        alu_control_code = {8'hff, datamux[3:0], 4'h8, 3'b100};
-                        data_alu = arga;
-                    end
-                    `ADD: begin
-                        io_control_code = 2'b00;
-                        pc_control_code = 3'b010;
-                        dc_control_code = 4'b0010;
-                        ct_control_code = 13'b0;
-                        alu_control_code = {8'h1, datamux[3:0], arga[3:0], 3'b000};
-                        data_alu = 16'h0;
-                    end
                     `MOV_RR: begin
                         io_control_code = 2'b00;
                         pc_control_code = 3'b010;
                         dc_control_code = 4'b0010;
-                        ct_control_code = 13'b0;
-                        alu_control_code = {8'hff, datamux[3:0], arga[3:0], 3'b000};
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], argb[3:0], arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `LOAD: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, datamux[3:0], 3'b010};
+                        addr = 16'b0;
+                        data_alu = arga;
+                    end
+                    `ADD_RR: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], datamux[3:0], arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `ADD_RI: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h8, arga[3:0], 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `SUB_RR: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], datamux[3:0], arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `SUB_RI: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h8, arga[3:0], 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `AND_RR: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], datamux[3:0], arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `AND_RI: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h8, arga[3:0], 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `OR_RR: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], datamux[3:0], arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `OR_RI: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h8, arga[3:0], 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `NOT_RR: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], datamux[3:0], arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `NOT_RI: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h8, arga[3:0], 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `XOR_RR: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], datamux[3:0], arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `XOR_RI: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h8, arga[3:0], 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `RAND_R: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `RAND_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `ROR_R: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `ROR_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `RXOR_R: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `RXOR_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `LSL_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `LSR_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `ASL_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `ASR_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `CSL_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `CSR_I: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, 4'h8, 3'b010};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `INC: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `DEC: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b010;
+                        dc_control_code = 4'b0010;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {inst[7:0], 4'h0, arga[3:0], 3'b000};
+                        addr = 16'b0;
+                        data_alu = 16'h0;
+                    end
+                    `JMP: begin
+                        io_control_code = 2'b00;
+                        pc_control_code = 3'b001;
+                        dc_control_code = 4'b0110;
+                        ct_control_code = {1'b0, 5'b0, 1'b0, 2'b00, 4'b0000};
+                        alu_control_code = {8'b0, 4'h0, 4'h0, 3'b000};
+                        addr = datamux;
                         data_alu = 16'h0;
                     end
                 endcase
